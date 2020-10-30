72bae3394032ae1aa6ae0780ec104ae356312584
llukkah <llukkah.rey@gmail.com>
2020-10-30 14:08:00 -0400

    added score board and correct answer option

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -48,6 +48,7 @@ export default class Questions extends Component {
             return {
                 questions: triviaData[currentQuestion].question,
                 options: triviaData[currentQuestion].incorrect,
+                correctAnswer: triviaData[currentQuestion].correct,
                 answers: triviaData[currentQuestion].correct
             }
         })
@@ -59,10 +60,17 @@ export default class Questions extends Component {
     }
 
     nextQuestionHandler = () => {
+        const{userAnswer, answers, score} = this.state;
         this.setState({
             currentQuestion: this.state.currentQuestion +1
         })
         console.log(this.state.currentQuestion)
+        //incrememnt score if answer is correct
+        if (userAnswer === answers) {
+            this.setState({
+                score: score + 1
+            })
+        }
     }
 //updates the component
 componentDidUpdate(prevProps, prevState){
@@ -70,8 +78,10 @@ componentDidUpdate(prevProps, prevState){
     if(this.state.currentQuestion !== prevState.currentQuestion) {
         this.setState(() => {
             return {
+                disabled: true,
                 questions: triviaData[currentQuestion].question,
                 options: triviaData[currentQuestion].incorrect,
+                correctAnswer: triviaData[currentQuestion].correct,
                 answers: triviaData[currentQuestion].correct
             }
         })
@@ -81,7 +91,8 @@ componentDidUpdate(prevProps, prevState){
 //check answer
 checkAnswer = answer => {
     this.setState({
-        userAnswer: answer
+        userAnswer: answer,
+        disabled: false
     })
 }
 
@@ -93,11 +104,24 @@ finishHandler = () => {
     }
 }
     render() {
-        const {questions, options, currentQuestion, userAnswer, quizEnd} = this.state;
+        const {questions, options, currentQuestion, correctAnswer, userAnswer, quizEnd} = this.state;
         if (quizEnd) {
             return (
                 <div>
                     <h2> Game Over.</h2>
+                    <h2>Your score is {this.state.score} points</h2>
+                    <p>The correct answers are:</p>
+                    <ul>
+                        {triviaData.map((item, index) => (
+                            <>
+                            <li className="ui floating message options"
+                            key={index}>{item.question}</li>
+                            <li className="ui floating message options"
+                            key={index}>{item.correct}</li>
+                            </>
+                        ))}
+                    </ul>
+                    {}
                 </div>
             )
         }
@@ -108,41 +132,56 @@ finishHandler = () => {
                     <h3>{questions}</h3>
                     <h5>{`Question ${currentQuestion} out of ${triviaData.length -1}`}</h5>
                 </section>
+                
                 {options.map(option => (
-                <ul className="list-items">
-                    <button
-                    variant="outline-light"
-                    className= {`ui floating message options
-                    ${userAnswer === option ? "selected" : null}
-                    `}
-                    onClick={() => this.checkAnswer(option)}
-                    >
-                    {option}
-                </button>
-                </ul>
+                    <ul className="list-items">
+                        <button
+                                variant="outline-light"
+                                className= {`ui floating message options
+                                    ${userAnswer === option ? "selected" : null}
+                                    `}
+                                onClick={() => this.checkAnswer(option)}
+                        >
+                            {option}
+                        </button>
+                    </ul>
                 ))}
+                {/* /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                CORRECT ANSWER NEEDS TO ADD TO SCORE 
+                /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                ///////////////////////////////////// */}
+                <button
+                        variant="outline-light"
+                        onClick={() => this.checkAnswer(userAnswer)}>
+                    {correctAnswer}
+                </button>
+                {/* /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                /////////////////////////////////////
+                ///////////////////////////////////// */}
+                
                 {currentQuestion < triviaData.length -1 && 
-
-
-
-                <button
-                onClick={this.nextQuestionHandler}>Next</button>
+                    <button
+                            disabled={this.setState.disabled}
+                            onClick={this.nextQuestionHandler}>
+                        Next
+                    </button>
                 }
+
                 {currentQuestion === triviaData.length -1 && 
-                <button
-                onClick={this.finishHandler}
-                > Finish</button>}
+                    <button
+                            onClick={this.finishHandler}
+                    >
+                        Finish
+                    </button>}
             </div>
         )
     }
 }
-
-//         <section className="outerCard">
-//             <p>{triviaQs.question}</p>
-//             <ul className="list-items">
-//                 <li><Button variant="outline-dark">{triviaQs.incorrect[0]}</Button></li>
-//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[1]}</Button></li>
-//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[2]}</Button></li>
-//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.correct}</Button></li>
-//             </ul>
-//         </section>

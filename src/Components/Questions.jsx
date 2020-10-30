ac05b61f00d672cd6f6cdae9dd7b9edb3bc66225
llukkah <llukkah.rey@gmail.com>
2020-10-30 12:13:23 -0400

    set state to render 1 question at a time

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -5,19 +5,20 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import './Question.css'
+import {TriviaData} from './triviaData'
 
-const triviaQ = triviaData.map((triviaQs) => {
-    return(
-        <>
-        <section className="outerCard">
-            <p>{triviaQs.question}</p>
-            <ul className="list-items">
-                <li><Button variant="outline-dark">{triviaQs.incorrect[0]}</Button></li>
-                <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[1]}</Button></li>
-                <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[2]}</Button></li>
-                <li><Button variant="outline-dark" className="button-style">{triviaQs.correct}</Button></li>
-            </ul>
-        </section>
+// const triviaQ = triviaData.map((triviaQs) => {
+//     return(
+//         <>
+//         <section className="outerCard">
+//             <p>{triviaQs.question}</p>
+//             <ul className="list-items">
+//                 <li><Button variant="outline-dark">{triviaQs.incorrect[0]}</Button></li>
+//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[1]}</Button></li>
+//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.incorrect[2]}</Button></li>
+//                 <li><Button variant="outline-dark" className="button-style">{triviaQs.correct}</Button></li>
+//             </ul>
+//         </section>
         {/* <Card className="outerCard">
             <Card.Header>{triviaQs.question}</Card.Header>
             <ListGroup className="list-items" as="ul" variant= "flush">
@@ -27,15 +28,65 @@ const triviaQ = triviaData.map((triviaQs) => {
                 <ListGroup.Item><Button variant="outline-dark">{triviaQs.correct}</Button></ListGroup.Item>
             </ListGroup>
         </Card> */}
-        </>
-    )
-})
+//         </>
+//     )
+// })
 
 export default class Questions extends Component {
+    state = {
+        userAnswer: null,
+        currentQuestion: 0,
+        options: []
+    }
+
+    loadQuiz = () => {
+        const{currentQuestion} = this.state;
+        this.setState(() => {
+            return {
+                questions: triviaData[currentQuestion].question,
+                options: triviaData[currentQuestion].incorrect,
+                answers: triviaData[currentQuestion].correct
+            }
+        })
+    }
+
+
+    componentDidMount() {
+        this.loadQuiz();
+    }
+
+    nextQuestionHandler = () => {
+        this.setState({
+            currentQuestion: this.state.currentQuestion +1
+        })
+        console.log(this.state.currentQuestion)
+    }
+//updates the component
+componentDidUpdate(prevProps, prevState){
+    const {currentQuestion} = this.state;
+    if(this.state.currentQuestion !== prevState.currentQuestion) {
+        this.setState(() => {
+            return {
+                questions: triviaData[currentQuestion].question,
+                options: triviaData[currentQuestion].incorrect,
+                answers: triviaData[currentQuestion].correct
+            }
+        })
+    }
+}
     render() {
+        const {questions, options} = this.state;
         return (
             <div className="finalDiv">
-            {triviaQ}
+                <p>curly bracket Questions:</p>{questions}
+                {options.map(option => (
+                <p>
+                    <p>mapped options:</p>
+                    {option}
+                </p>
+                ))}
+                <button
+                onClick={this.nextQuestionHandler}>Next</button>
             </div>
         )
     }
